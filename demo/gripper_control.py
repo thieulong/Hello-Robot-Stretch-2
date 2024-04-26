75ed460d73ec3db65107cd9077465ef157e9a1ab
thieulong <thieulongph@gmail.com>
2024-04-26 14:45:23 +1000

    Testing

--- demo/gripper_control.py
+++ demo/gripper_control.py
@@ -2,38 +2,54 @@
 
 import rospy
 import time
-from std_msgs.msg import Bool
-from stretch_body.robot import Robot
+from std_msgs.msg import Bool, Float32
+import stretch_body.robot
+import stretch_body.arm
 
 class StretchLiftController:
     def __init__(self):
         rospy.init_node('stretch_lift_controller')
         
-        # Initialize Stretch RE2 robot
-        self.robot = Robot()
-        self.robot.startup()
+        self.robot = stretch_body.robot.Robot()
+        self.arm = stretch_body.arm.Arm()
+        self.arm.motor.disable_sync_mode()
 
-        # Subscribers
-        rospy.Subscriber("/object_in_hand", Bool, self.handle_object_in_hand)
+        if not self.robot.start_up():
+            rospy.logerr("Failed to initialize Stretch Body")
+        if not self.arm.startup():
+            rospy.logerr("Failed to initialize Stretch Arm")
+
+        rospy.Subscriber("/lift_object", Bool, self.handle_lift_object)
         rospy.Subscriber("/release_object", Bool, self.handle_release_object)
 
-    def handle_object_in_hand(self, data):
+        rospy.Subscriber("/move_lift", Float32, self.handle_move_lift)
+        rospy.Subscriber("/move_arm", Float32, self.handle_move_arm)
+
+        self.lift_pub = rospy.Publisher("/move_lift", Float32, queue_size=1)
+        self.arm_pub = rospy.Publisher("/move_arm", Float32, queue_size=1)
+
+    def handle_lift_object(self, data):
         if data.data:
             rospy.loginfo("Received signal to lift object.")
-            # Lift the lift upwards to a certain position
-            self.robot.lift.move_to(0.5)  # Adjust the target position as needed
-            self.robot.push_command()
+            self.lift_pub.publish(Float32(1.0))
+            rospy.loginfo("Object lifted!")
 
     def handle_release_object(self, data):
         if data.data:
             rospy.loginfo("Received signal to release object.")
-            # Move the lift downwards to the original position
-            self.robot.lift.move_to(0.0)  # Adjust the target position as needed
-            self.robot.push_command()
-            time.sleep(2)
-            # Open the gripper after 2 seconds
-            self.robot.end_of_arm.move_to('stretch_gripper', {'pos': 0.0})  # Fully open
-            self.robot.push_command()
+            self.lift_pub.publish(Float32(0.7))
+            self.arm_pub.publish(Float32(0.35))
+
+    def handle_move_lift(self, data):
+        rospy.loginfo(f"Received command to move lift to position: {data.data}")
+        self.robot.lift.move_to(data.data) 
+        self.robot.push_command()
+
+    def handle_move_arm(self, data):
+        rospy.loginfo(f"Received command to move arm to position: {data.data}")
+        self.arm.move_to(data.data)
+        self.arm.push_command()
+        self.arm.motor.wait_until_at_setpoint()
 
     def run(self):
         rospy.spin()
