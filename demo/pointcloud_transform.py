aea002b1f381fc1f4cea4d20162bb865d4ddffdc
Thieu Long <59945736+thieulong@users.noreply.github.com>
2024-06-17 03:08:48 +1000

    Update pointcloud_transform.py

--- demo/pointcloud_transform.py
+++ demo/pointcloud_transform.py
@@ -1,14 +1,12 @@
 #!/usr/bin/env python3
-
-# Import modules
 import rospy
 import tf
 import sensor_msgs.point_cloud2 as pc2
-
-# Import message types and other python libraries
 from sensor_msgs.msg import PointCloud2, PointCloud
 from geometry_msgs.msg import Point32
 from std_msgs.msg import Header
+import open3d as o3d
+import numpy as np
 
 class PointCloudTransformer:
     """
@@ -18,25 +16,16 @@ class PointCloudTransformer:
     """
     def __init__(self):
         """
-        Function that initializes the subsriber, publisher, and other variables.
+        Function that initializes the subscriber, publisher, and other variables.
         :param self: The self reference.
         """
-        # Initialize Subscriber
         self.pointcloud2_sub = rospy.Subscriber("/camera/depth/color/points", PointCloud2, self.callback_pcl2, queue_size=1)
-
-        # Initialize PointCloud Publisher
         self.pointcloud_pub = rospy.Publisher("/camera_cloud", PointCloud, queue_size=1)
-
-        # Initialize self.pcl2_cloud to store the PointCloud2
         self.pcl2_cloud = None
-
-        # Intialize tf.Transformlistener
         self.listener = tf.TransformListener(True, rospy.Duration(10.0))
-
-        # Create rospy log message
         rospy.loginfo('Publishing transformed PointCloud. Use RViz to visualize')
 
-    def callback_pcl2(self,msg):
+    def callback_pcl2(self, msg):
         """
         Callback function that stores the PointCloud2 message.
         :param self: The self reference.
@@ -48,61 +37,74 @@ class PointCloudTransformer:
         """
         A function that extracts the points from the stored PointCloud2 message
         and appends those points to a PointCloud message. Then the function transforms
-        the PointCloud from its header frame id, 'camera_color_optical_frame'
+        the PointCloud from its the header frame id, 'camera_color_optical_frame'
         to the 'base_link' frame.
         :param self: The self reference.
         """
-        # Create a PointCloud for temporary use. Set the temporary PointCloud's
-        # header to the stored PointCloud2 header
         temp_cloud = PointCloud()
         temp_cloud.header = self.pcl2_cloud.header
-
-        # For loop to extract PointCloud2 data into a list of x,y,z, points and
-        # append those values to the PointCloud message, temp_cloud
         for data in pc2.read_points(self.pcl2_cloud, skip_nans=True):
-            temp_cloud.points.append(Point32(data[0],data[1],data[2]))
+            temp_cloud.points.append(Point32(data[0], data[1], data[2]))
 
-        # Transform the points in the PointCloud message to reference the base_link
         transformed_cloud = self.transform_pointcloud(temp_cloud)
-
-        # Publish transformed PointCloud
         self.pointcloud_pub.publish(transformed_cloud)
 
-    def transform_pointcloud(self,msg):
+    def transform_pointcloud(self, msg):
         """
-        Function that stores the PointCloud2 message.
+        Function that transforms the PointCloud2 message.
         :param self: The self reference.
         :param msg: The PointCloud message.
-
         :returns new_cloud: The transformed PointCloud message.
         """
         while not rospy.is_shutdown():
             try:
-                # run the transformPointCloud() function to change the referene frame
-                # to the base_link
-                new_cloud = self.listener.transformPointCloud("base_link" ,msg)
+                new_cloud = self.listener.transformPointCloud("base_link", msg)
                 return new_cloud
                 if new_cloud:
                     break
-            except (tf.LookupException, tf.ConnectivityException,tf.ExtrapolationException):
+            except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
                 pass
 
-if __name__=="__main__":
-    # Initialize the node, and call it "pointcloud_transformer"
-    rospy.init_node('pointcloud_transformer',anonymous=True)
+    def plane_segmentation(self):
+        """
+        Function to perform plane segmentation on the PointCloud.
+        :param self: The self reference.
+        """
+        points = []
+        for data in pc2.read_points(self.pcl2_cloud, field_names=("x", "y", "z"), skip_nans=True):
+            points.append([data[0], data[1], data[2]])
+        
+        # Convert to Open3D point cloud
+        pcd = o3d.geometry.PointCloud()
+        pcd.points = o3d.utility.Vector3dVector(points)
 
-    # Instanstiate a `PointCloudTransformer()` object
+        # Perform plane segmentation
+        plane_model, inliers = pcd.segment_plane(distance_threshold=0.01, ransac_n=3, num_iterations=1000)
+
+        if len(inliers) == 0:
+            rospy.loginfo("Could not find any plane in the point cloud.")
+            return
+
+        # Extract inlier points
+        inlier_cloud = pcd.select_by_index(inliers)
+        inlier_points = np.asarray(inlier_cloud.points)
+
+        # Publish the segmented plane point cloud
+        plane_cloud = PointCloud()
+        plane_cloud.header = self.pcl2_cloud.header
+        for point in inlier_points:
+            plane_cloud.points.append(Point32(point[0], point[1], point[2]))
+
+        self.pointcloud_pub.publish(plane_cloud)
+        rospy.loginfo("Published segmented plane point cloud.")
+
+if __name__ == "__main__":
+    rospy.init_node('pointcloud_transformer', anonymous=True)
     PCT = PointCloudTransformer()
-
-    # We're going to publish information at 1 Hz
     rate = rospy.Rate(1)
-
-    # Give the listener some time to accumulate transforms
     rospy.sleep(1)
 
-    # Run while loop until the node is shutdown
     while not rospy.is_shutdown():
-
-        # Run the `pcl_transformer()` method
-        PCT.pcl_transformer()
+        PCT.pcl_transformer()  # Transform point cloud
+        PCT.plane_segmentation()  # Perform plane segmentation
         rate.sleep()
