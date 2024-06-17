0ab8c815042bf1721482459c785dacc2cd190c0a
thieulong <thieulongph@gmail.com>
2024-06-17 15:59:55 +1000

    Testing

--- demo/pointcloud_transform.py
+++ demo/pointcloud_transform.py
@@ -65,38 +65,45 @@ class PointCloudTransformer:
             except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
                 pass
 
-    def plane_segmentation(self):
-        """
-        Function to perform plane segmentation on the PointCloud.
-        :param self: The self reference.
-        """
-        points = []
-        for data in pc2.read_points(self.pcl2_cloud, field_names=("x", "y", "z"), skip_nans=True):
-            points.append([data[0], data[1], data[2]])
-        
-        # Convert to Open3D point cloud
-        pcd = o3d.geometry.PointCloud()
-        pcd.points = o3d.utility.Vector3dVector(points)
+def plane_segmentation(self):
+    """
+    Function to perform plane segmentation on the PointCloud.
+    :param self: The self reference.
+    """
+    points = []
+    for data in pc2.read_points(self.pcl2_cloud, field_names=("x", "y", "z"), skip_nans=True):
+        points.append([data[0], data[1], data[2]])
+    
+    # Convert to Open3D point cloud
+    pcd = o3d.geometry.PointCloud()
+    pcd.points = o3d.utility.Vector3dVector(points)
 
-        # Perform plane segmentation
-        plane_model, inliers = pcd.segment_plane(distance_threshold=0.01, ransac_n=3, num_iterations=500)
+    # Perform plane segmentation
+    plane_model, inliers = pcd.segment_plane(distance_threshold=0.01, ransac_n=3, num_iterations=500)
 
-        if len(inliers) == 0:
-            rospy.loginfo("Could not find any plane in the point cloud.")
-            return
+    if len(inliers) == 0:
+        rospy.loginfo("Could not find any plane in the point cloud.")
+        return
 
-        # Extract inlier points
-        inlier_cloud = pcd.select_by_index(inliers)
-        inlier_points = np.asarray(inlier_cloud.points)
+    # Extract inlier points
+    inlier_cloud = pcd.select_by_index(inliers)
+    inlier_points = np.asarray(inlier_cloud.points)
 
-        # Publish the segmented plane point cloud
-        plane_cloud = PointCloud()
-        plane_cloud.header = self.pcl2_cloud.header
-        for point in inlier_points:
-            plane_cloud.points.append(Point32(point[0], point[1], point[2]))
+    # Filter points with z > 0.3m
+    filtered_points = inlier_points[inlier_points[:, 2] > 0.3]
 
-        self.pointcloud_pub.publish(plane_cloud)
-        rospy.loginfo("Published segmented plane point cloud.")
+    if len(filtered_points) == 0:
+        rospy.loginfo("No points found with z > 0.3m in the segmented plane point cloud.")
+        return
+
+    # Publish the filtered point cloud
+    plane_cloud = PointCloud()
+    plane_cloud.header = self.pcl2_cloud.header
+    for point in filtered_points:
+        plane_cloud.points.append(Point32(point[0], point[1], point[2]))
+
+    self.pointcloud_pub.publish(plane_cloud)
+    rospy.loginfo("Published segmented plane point cloud with z > 0.3m.")
 
 if __name__ == "__main__":
     rospy.init_node('pointcloud_transformer', anonymous=True)
